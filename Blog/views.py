9f138b90cf99306f561e7250f2a719a12b9c6b67
Qasimsaifi <106295428+Qasimsaifi@users.noreply.github.com>
2023-06-07 12:43:42 +0530

    sdfd

--- Blog/views.py
+++ Blog/views.py
@@ -14,9 +14,10 @@ class BlogPostPagination(PageNumberPagination):
     page_size = 6
 
 class BlogPostViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = BlogPost.objects.all()
+    queryset = BlogPost.objects.all().order_by('publication_date')
     serializer_class = BlogPostSerializer
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend, OrderingFilter]
     filterset_class = BlogPostFilter
     ordering_fields = ['publication_date']
     pagination_class = BlogPostPagination
+
